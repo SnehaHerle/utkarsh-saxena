bd1f3b74d7d5068b6bd031afa495ff4383dae009
Sneha Herle <sneha.herle@gmail.com>
2020-10-11 19:37:55 +0530

    fix routing issue with gh-pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
+import ReactDom from 'react-dom'
+import {HashRouter, BrowserRouter, Route, Link, Switch} from "react-router-dom"
 import Navbar from './components/Navbar'
 import "./App.css"
-import {BrowserRouter, Route} from 'react-router-dom'
+//import {BrowserRouter, Route} from 'react-router-dom'
 import About from '../src/components/screens/About'
 import Inspiration from '../src/components/screens/Inspiration'
 import Story from '../src/components/screens/Story'
 import Footer from '../src/components/screens/Footer'
+import { render } from '@testing-library/react';
 
 
 function App() {
   return (
     
-    <BrowserRouter>
+    <BrowserRouter basename = {process.env.PUBLIC_URL}>
         <Navbar/>
         <Route exact path = "/utkarsh-saxena">
           <About />
